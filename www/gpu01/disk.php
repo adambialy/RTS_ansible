4ae700c95535a459e68d9820ed0135ec4ebbd725
root <root@rts>
2023-10-31 15:49:50 +0000

    php index change

--- www/gpu01/disk.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$file = 'gpu01-disk-test-1.html';
-if($f = fopen($file, 'r')){
-  $line = fgets($f); 
-  fclose($f);
-}
-echo $line;
-?>
--- www/index.php
+++ www/index.php
@@ -31,14 +31,7 @@
 </thead>
 <tbody>
 
-<?php include 'control/control-index.php'; ?>
-<?php include 'gpu01/gpu01-index.php'; ?>
-<?php include 'gpu02/gpu02-index.php'; ?>
-<?php include 'gpu03/gpu03-index.php'; ?>
-<?php include 'gpu04/gpu04-index.php'; ?>
-<?php include 'gpu05/gpu05-index.php'; ?>
-<?php include 'gpu06/gpu06-index.php'; ?>
-<?php include 'gpu07/gpu07-index.php'; ?>
+<?php include 'servers.php'; ?>
 
 </tbody>
 </table>
