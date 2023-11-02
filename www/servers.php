ed317dd18691fd6d566a8d2745871eaae2f990db
root <root@rts>
2023-11-02 23:56:57 +0000

    bits and corrections

--- www/servers.php
+++ www/servers.php
@@ -1,14 +1,33 @@
 <?php
+foreach (glob("control/*.php") as $filename)
+{ include $filename; }
+?>
+<?php
 foreach (glob("gpu01/*.php") as $filename)
 { include $filename; }
 ?>
+<?php
+foreach (glob("gpu02/*.php") as $filename)
+{ include $filename; }
+?>
+<?php
+foreach (glob("gpu03/*.php") as $filename)
+{ include $filename; }
+?>
+<?php
+foreach (glob("gpu04/*.php") as $filename)
+{ include $filename; }
+?>
+<?php
+foreach (glob("gpu05/*.php") as $filename)
+{ include $filename; }
+?>
+<?php
+foreach (glob("gpu06/*.php") as $filename)
+{ include $filename; }
+?>
+<?php
+foreach (glob("gpu07/*.php") as $filename)
+{ include $filename; }
+?>
 
-
-<?php include 'control/control-index.php'; ?>
-<?php include 'gpu01/gpu01-index.php'; ?>
-<?php include 'gpu02/gpu02-index.php'; ?>
-<?php include 'gpu03/gpu03-index.php'; ?>
-<?php include 'gpu04/gpu04-index.php'; ?>
-<?php include 'gpu05/gpu05-index.php'; ?>
-<?php include 'gpu06/gpu06-index.php'; ?>
-<?php include 'gpu07/gpu07-index.php'; ?>
