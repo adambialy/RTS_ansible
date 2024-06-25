01081115ed0527048ac7552974966b5da96c7433
Adam Bialy <adam.bialy.me@gmail.com>
2024-06-25 20:20:43 +0100

    20240625

--- run_tests.sh
+++ run_tests.sh
@@ -2,12 +2,12 @@
 
 if [ -z "${1}" ]; then
 	ansible-playbook -i inventory/hosts.yaml srv_info.yaml -l gpu
-	ansible-playbook -i inventory/hosts.yaml disk_test.yaml -l gpu
-	ansible-playbook -i inventory/hosts.yaml iperf.yaml -l gpu
+	ansible-playbook -i inventory/hosts.yaml storage_test.yaml -l gpu
+	ansible-playbook -i inventory/hosts.yaml network_test.yaml -l gpu
 else
 	ansible-playbook -i inventory/hosts.yaml srv_info.yaml -l ${1}
-	ansible-playbook -i inventory/hosts.yaml disk_test.yaml -l ${1}
- 	ansible-playbook -i inventory/hosts.yaml iperf.yaml -l ${1}
+	ansible-playbook -i inventory/hosts.yaml storage_test.yaml -l ${1}
+ 	ansible-playbook -i inventory/hosts.yaml network_test.yaml -l ${1}
 fi
 
 
---
+++ www/gpu01-index.php
@@ -0,0 +1,5 @@
+<?php include 'hdparm-T.php'; ?>
+<?php include 'hdparm-t.php'; ?>
+<?php include 'iperf-std-receiver.php'; ?>
+<?php include 'iperf-std-sender.php'; ?>
+<?php include 'gpu-burn.php'; ?>
