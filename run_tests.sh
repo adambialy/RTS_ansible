078da7f12504ad63eb0ef70ea01b0185e087d51c
root <root@debian11.bialy.in>
2024-07-02 22:47:49 +0100

    20240702_1 - working copy; TODO add raid check

--- run_tests.sh
+++ run_tests.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 if [ -z "${1}" ]; then
-	ansible-playbook -i inventory/hosts.yaml srv_info.yaml -l gpu
-	ansible-playbook -i inventory/hosts.yaml storage_test.yaml -l gpu
-	ansible-playbook -i inventory/hosts.yaml network_test.yaml -l gpu
+	ansible-playbook -i hosts.yaml identify.yaml
+	ansible-playbook -i hosts.yaml srv_info.yaml
+	ansible-playbook -i hosts.yaml storage_test.yaml
+	ansible-playbook -i hosts.yaml network_test.yaml -l gpu
+	ansible-playbook -i hosts.yaml gpu_test.yaml -l gpu
 else
-	ansible-playbook -i inventory/hosts.yaml srv_info.yaml -l ${1}
-	ansible-playbook -i inventory/hosts.yaml storage_test.yaml -l ${1}
- 	ansible-playbook -i inventory/hosts.yaml network_test.yaml -l ${1}
+	ansible-playbook -i hosts.yaml identify.yaml -l ${1}
+	ansible-playbook -i hosts.yaml srv_info.yaml -l ${1}
+	ansible-playbook -i hosts.yaml storage_test.yaml -l ${1}
+ 	ansible-playbook -i hosts.yaml network_test.yaml -l ${1}
+ 	ansible-playbook -i hosts.yaml gpu_test.yaml -l ${1}
 fi
 
-
+curl http://localhost | html2text > /var/www/html/index.txt
